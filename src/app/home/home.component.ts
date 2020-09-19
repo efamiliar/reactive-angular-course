80b57e337ef18d21f192da29ff74f1f7626d4cff
Esteban Familiar <efamiliar@gmail.com>
2020-09-20 01:35:44 +0200

    leccion 2-9

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareRep
 import {HttpClient} from '@angular/common/http';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
-
+import {CoursesService} from '../services/courses.service';
 
 @Component({
   selector: 'home',
@@ -14,44 +14,28 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 })
 export class HomeComponent implements OnInit {
 
-  beginnerCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private http: HttpClient, private dialog: MatDialog) {
+  constructor(private courseService: CoursesService) {
 
   }
 
   ngOnInit() {
+    const courses$ = this.courseService.loadAllCourses()
+     .pipe(map(courses => courses.sort(sortCoursesBySeqNo)));
+    this.beginnerCourses$ = courses$.pipe(map(courses => courses
+      .filter(course => course
+        .category=="BEGINNER")));
 
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
+    this.advancedCourses$ = courses$.pipe(map(courses => courses
+          .filter(course => course
+            .category=="ADVANCED")));
 
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
-
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-        });
-
-  }
-
-  editCourse(course: Course) {
-
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
-  }
+  }    
+ 
 
 }
 
