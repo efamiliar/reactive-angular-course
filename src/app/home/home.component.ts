be2c231eb988907dd8e71983a94ce6b4a292782d
Esteban Familiar <efamiliar@gmail.com>
2020-09-20 11:37:36 +0200

    Finde la segunda seccion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {Course, sortCoursesBySeqNo} from '../model/course';
-import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
-import {CoursesService} from '../services/courses.service';
+import { Component, OnInit } from '@angular/core';
+import { Course, sortCoursesBySeqNo } from '../model/course';
+import { interval, noop, Observable, of, throwError, timer } from 'rxjs';
+import { catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'home',
@@ -24,18 +24,22 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.reloadCourses();
+  }
+
+  reloadCourses() {
     const courses$ = this.courseService.loadAllCourses()
-     .pipe(map(courses => courses.sort(sortCoursesBySeqNo)));
+      .pipe(map(courses => courses.sort(sortCoursesBySeqNo)));
     this.beginnerCourses$ = courses$.pipe(map(courses => courses
       .filter(course => course
-        .category=="BEGINNER")));
+        .category == "BEGINNER")));
 
     this.advancedCourses$ = courses$.pipe(map(courses => courses
-          .filter(course => course
-            .category=="ADVANCED")));
+      .filter(course => course
+        .category == "ADVANCED")));
 
-  }    
- 
+  }
+
 
 }
 
